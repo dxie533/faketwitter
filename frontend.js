c46ab8b811c364585df9394f593ff7de99246f96
notcoollol <cool75739@gmail.com>
2019-11-02 22:27:26 -0400

    More debug messages.

--- frontend.js
+++ frontend.js
@@ -413,7 +413,7 @@ router.post("/follow",async function(req,res){
 	var followJSON = {};
 	var username = req.body.username;
 	var following = req.body.follow;
-	console.log(username);
+	
 	if(following == undefined){
 		following = true;
 	}
@@ -443,6 +443,7 @@ router.post("/follow",async function(req,res){
 				return;
 			}
 			followJSON.originUsername = decoded.username;
+			console.log(decoded.username);
 			followingArray = decoded.following; 
 		}catch(err){
 			responseJSON.status = "error";
@@ -471,7 +472,7 @@ router.post("/follow",async function(req,res){
 					return;
 				}else{
 					var newToken = jwt.sign({username:followJSON.originUsername,following:followingArray},secretToken,{expiresIn: 86400});
-					console.log("Token from followJSON"followJSON.originUsername);
+					console.log("Token from followJSON"+followJSON.originUsername);
 					res.cookie('token', newToken, {maxAge: 86400*1000, overwrite: true});
 					responseJSON.status = "OK";
 					res.status(200).send(body);
